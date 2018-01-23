dc489764e12b7c5a68bf9a66c1b56ffbb387a603
queicherius <queicherius@gmail.com>
2018-01-23 20:04:20 +0000

    Initial implementation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,48 @@
-const answer = 42
+const pattern = /(-)?("[^"]*"|[^\s]+)/g
 
-// Super simple demo function to return the answer of life
-export default async function () {
-  return answer
+export function tokenize (query) {
+  query = query.toLowerCase().trim()
+
+  let results = []
+  let matched
+
+  // eslint-disable-next-line
+  while (matched = pattern.exec(query)) {
+    const prefix = matched[1]
+    let term = matched[2]
+    let result = {}
+
+    // Strip quotes
+    term = term.replace(/(^"|"$)/g, '')
+    result.term = term
+
+    // Set flags based on prefix
+    if (prefix === '-') {
+      result.exclude = true
+    }
+
+    results.push(result)
+  }
+
+  // Sort the results so the terms with the exclude flag are at the very start
+  results.sort((a, b) => (a.exclude === b.exclude) ? 0 : a.exclude ? -1 : 1)
+
+  return results
+}
+
+export function match (tokens, text) {
+  for (let i = 0; i !== tokens.length; i++) {
+    const token = tokens[i]
+    const match = text.indexOf(token.term) !== -1
+
+    if (token.exclude && match) {
+      return false
+    }
+
+    if (!token.exclude && !match) {
+      return false
+    }
+  }
+
+  return true
 }
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,17 +1,107 @@
 /* eslint-env node, mocha */
-import {expect} from 'chai'
-import module from '../src/index.js'
+import { expect } from 'chai'
+import * as module from '../src/index.js'
 
-describe('my module', () => {
-  it('should return the answer to life', async () => {
-    let result = await module()
-    expect(result).to.equal(42)
+const TEXT = `
+Up am intention on dependent questions oh elsewhere september.
+No betrayed pleasure possible jointure we in throwing.
+And can event rapid any shall woman green.
+`
+
+function match (query) {
+  query = module.tokenize(query)
+  return module.match(query, TEXT)
+}
+
+describe('tokenize', () => {
+  it('single word', async () => {
+    expect(module.tokenize(' Dependent   ')).to.deep.equal([
+      {term: 'dependent'}
+    ])
   })
 
-  it('should be able to overwrite internals', async () => {
-    module.__set__('answer', 'programming!')
+  it('multiple words', async () => {
+    expect(module.tokenize('on dependent')).to.deep.equal([
+      {term: 'on'},
+      {term: 'dependent'}
+    ])
+  })
 
-    let result = await module()
-    expect(result).to.equal('programming!')
+  it('multiple words with lots of spaces', async () => {
+    expect(module.tokenize('   on          dependent       ')).to.deep.equal([
+      {term: 'on'},
+      {term: 'dependent'}
+    ])
+  })
+
+  it('single phrase', async () => {
+    expect(module.tokenize('"on dependent"')).to.deep.equal([
+      {term: 'on dependent'}
+    ])
+  })
+
+  it('multiple phrases', async () => {
+    expect(module.tokenize('"on dependent" "herp di derp" "flerp"')).to.deep.equal([
+      {term: 'on dependent'},
+      {term: 'herp di derp'},
+      {term: 'flerp'}
+    ])
+  })
+
+  it('exclude', async () => {
+    expect(module.tokenize('-herp -"herp derp"')).to.deep.equal([
+      {term: 'herp', exclude: true},
+      {term: 'herp derp', exclude: true}
+    ])
+  })
+
+  it('kitchen sink', async () => {
+    expect(module.tokenize('"Hello World" my dear -"how are you" -horrible oh')).to.deep.equal([
+      {term: 'how are you', exclude: true},
+      {term: 'horrible', exclude: true},
+      {term: 'hello world'},
+      {term: 'my'},
+      {term: 'dear'},
+      {term: 'oh'}
+    ])
+  })
+})
+
+describe('match', () => {
+  it('single word', async () => {
+    expect(match('dependent')).to.equal(true)
+    expect(match('herp')).to.equal(false)
+  })
+
+  it('multiple words', async () => {
+    expect(match('dependent pleasure')).to.equal(true)
+    expect(match('dependent herp')).to.equal(false)
+    expect(match('herp derp')).to.equal(false)
+  })
+
+  it('single phrase', async () => {
+    expect(match('"dependent questions"')).to.equal(true)
+    expect(match('"dependent pleasure"')).to.equal(false)
+  })
+
+  it('multiple phrases', async () => {
+    expect(match('"dependent questions" "in throwing"')).to.equal(true)
+    expect(match('"dependent pleasure" "in throwing"')).to.equal(false)
+    expect(match('"dependent pleasure" "in throwing" "shall green"')).to.equal(false)
+  })
+
+  it('exclude', async () => {
+    expect(match('-herp')).to.equal(true)
+    expect(match('-"possible throwing"')).to.equal(true)
+    expect(match('-pleasure')).to.equal(false)
+    expect(match('-"pleasure possible"')).to.equal(false)
+  })
+
+  it('kitchen sink', async () => {
+    expect(match('"possible jointure" -"herp derp" -derp woman')).to.equal(true)
+    expect(match('-"possible jointure" -"herp derp" -derp woman')).to.equal(false)
+    expect(match('"possible jointure" -"herp derp" -derp -woman')).to.equal(false)
+    expect(match('"possible jointure" "herp derp" -derp woman')).to.equal(false)
+    expect(match('"possible jointure" -"herp derp" derp woman')).to.equal(false)
   })
 })
